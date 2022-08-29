554017dd7539be6432e3343aa5cc71149d1e1de9
mishyVT <white.skidmore@gmail.com>
2022-08-29 09:43:42 -0600

    Added credits

--- res/js/load-email.js
+++ res/js/load-email.js
@@ -1,7 +1,6 @@
 document.getElementById("email").addEventListener("click", function (evt) {
     let odd = "jhahnwy.pc", even = "onto@itsae", text = "";
     for(let i = 0; i < odd.length; i++) { text += odd[i] + even[i]; }
-    text = "<a href=\"mailto:johnathon@wiyt.space\">" + text + "</a>";
     evt.target.classList.remove("unloaded");
-    evt.target.innerHTML = text;
+    evt.target.innerHTML = "<a href=\"mailto:" + text + "\">" + text + "</a>";
 });

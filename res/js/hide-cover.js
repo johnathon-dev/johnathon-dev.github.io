3c2274e8201877a6e3540dbb121fa53ff8122b98
mishyVT <white.skidmore@gmail.com>
2022-08-29 00:23:00 -0600

    Cover uses sessionStorage

--- res/js/hide-cover.js
+++ res/js/hide-cover.js
@@ -1,6 +1,12 @@
 const cover = document.getElementById('ws-cover');
-cover.addEventListener('click', function() {
+let covered = sessionStorage.getItem("uncovered");
+
+function clicked() {
     cover.style.opacity = '0';
     cover.style.visibility = 'hidden';
     document.body.classList.add('scrolling');
-});
+    sessionStorage.setItem("uncovered", "true");
+}
+
+if(covered === 'true') clicked();
+else cover.addEventListener('click', clicked);

3d870c608a36a08d2cfbe587af84d9ffa0751100
mishyVT <white.skidmore@gmail.com>
2022-08-29 09:17:25 -0600

    Removed sessionStorage cover

--- res/js/load-email.js
+++ res/js/load-email.js
@@ -1,6 +1,7 @@
 document.getElementById("email").addEventListener("click", function (evt) {
     let odd = "jhahnwy.pc", even = "onto@itsae", text = "";
     for(let i = 0; i < odd.length; i++) { text += odd[i] + even[i]; }
+    text = "<a href=\"mailto:johnathon@wiyt.space\">" + text + "</a>";
     evt.target.classList.remove("unloaded");
     evt.target.innerHTML = text;
 });

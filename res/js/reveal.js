a24d9d3f655660a68ba9fe6e97a96add2d96b4aa
mishyVT <white.skidmore@gmail.com>
2022-08-29 19:40:01 -0600

    Adjusted reveal.js

--- res/js/reveal.js
+++ res/js/reveal.js
@@ -5,9 +5,9 @@ function reveal() {
     const reveals = document.querySelectorAll('.reveal');
     for(let i = 0; i < reveals.length; i++) {
         const innerHeight = window.innerHeight, revealTop = reveals[i].getBoundingClientRect().top, revealPoint = 150;
-        let check = reveals[0].classList.contains('active') || reveals[1].classList.contains('active');
+        let checkFirst = reveals[0].classList.contains('active');
         reveals[i].classList.toggle('active', revealTop < (innerHeight - revealPoint));
-        if(check) {
+        if(checkFirst) {
             document.getElementById('scroll').style.opacity = "0";
             document.getElementById('scroll').style.marginTop = "0px";
         } else {

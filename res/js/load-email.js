90cf846b8d9febeb7462d1bb984f7b671d3eb2f2
mishyVT <white.skidmore@gmail.com>
2022-09-25 13:07:59 -0600

    Changed email temporarily

--- res/js/load-email.js
+++ res/js/load-email.js
@@ -1,7 +1,7 @@
 document.getElementById("email").addEventListener("click", function (evt) {
     // let odd = "jhahnwy.pc", even = "onto@itsae", text = "";
     let odd = "wiesimr@codcm", even = "ht.kdoeilu.o", text = "";
-    for(let i = 0; i < odd.length; i++) { text += odd[i] + even[i]; }
+    for(let i = 0; i < 12; i++) { text += odd[i] + even[i]; }
     evt.target.classList.remove("unloaded");
-    evt.target.innerHTML = "<a href=\"mailto:" + text + "\">" + text + "</a>";
+    evt.target.innerHTML = "<a href=\"mailto:" + text + "m\">" + text + "m</a>";
 });
